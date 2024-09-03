eb583fd547fb559e27a62c32c669a02cc0525e96
Lakshmanun <Lakshminarayanans.yadav@gmail.com>
2024-09-03 11:01:05 +0530

    aedited files

--- e-cart/src/App.jsx
+++ e-cart/src/App.jsx
@@ -1,23 +1,11 @@
 
-import Home from './COMPONENTS/Home';
-import Card from './COMPONENTS/Card';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './COMPONENTS/Header';
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Header />
-
-      <Routes>
-
-        <Route path='/' element={<Home />} />
-        <Route path='/card' element={<Card />} />
-        
-      </Routes>
-    </BrowserRouter>
-
+  <div>
+    app.js
+  </div>
   );
 }
 
